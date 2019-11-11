e6e2cf7af4c8af64d6cb2360d2a5c6e92bcb044a
ra44o <ra44elo@gmail.com>
2019-11-11 18:26:03 +0300

    fix: rename local array, fix .gitignore

--- express-server-with-architecture/index.js
+++ express-server-with-architecture/index.js
@@ -11,19 +11,19 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-let users;
+let localUsers;
 fs.readFile(
   path.join(__dirname, 'storage.json'),
   (err, data) => {
     if (err) {
       throw new Error(err);
     }
-    users = JSON.parse(data).users;
+    localUsers = JSON.parse(data).users;
   }
 );
 
 app.get('/api/users/get-users', (req, res) => {
-  res.json(users);
+  res.json(localUsers);
   res.end();
 });
 
@@ -32,30 +32,30 @@ app.post('/api/users/create-user', (req, res) => {
     res.json({ msg: "Bad request" });
     res.end();
   } else {
-    if (users.some(user => user.id === req.body.id)) {
+    if (localUsers.some(user => user.id === req.body.id)) {
       res.json({ msg: `User with id ${req.body.id} exists` });
       res.end();
     } else {
-      users.push(req.body);
+      localUsers.push(req.body);
       fs.writeFile(
         'storage.json',
-        JSON.stringify({ users }),
+        JSON.stringify({ users: localUsers }),
         err => {
           throw new Error(err);
         }
       );
-      res.json(users);
+      res.json(localUsers);
       res.end();
     }
   }
 });
 
 app.put('/api/users/update-user', (req, res) => {
-  if (!users.some(user => user.id === req.body.id)) {
+  if (!localUsers.some(user => user.id === req.body.id)) {
     res.json({ msg: `User with id ${req.body.id} does not exist` });
     res.end();
   } else {
-    const currentUser = users.filter(user => user.id === req.body.id)[0];
+    const currentUser = localUsers.filter(user => user.id === req.body.id)[0];
     if (req.body.name) {
       currentUser.name = req.body.name;
     } else if (req.body.surname) {
@@ -63,32 +63,32 @@ app.put('/api/users/update-user', (req, res) => {
     }
     fs.writeFile(
       'storage.json',
-      JSON.stringify({ users }),
+      JSON.stringify({ users: localUsers }),
       err => {
         throw new Error(err);
       }
     );
-    res.json(users);
+    res.json(localUsers);
     res.end();
   }
 });
 
 app.delete('/api/users/delete-user', (req, res) => {
   // we must pass the whole object to delete it
-  if (users.some(user => user.id === req.body.id)) {
+  if (localUsers.some(user => user.id === req.body.id)) {
     res.json({ msg: `User with id ${req.body.id} does not exist` });
     res.end();
   } else {
-    const currentUserPosition = users.indexOf(req.body.user);
-    users.splice(currentUserPosition, 1);
+    const currentUserPosition = localUsers.indexOf(req.body.user);
+    localUsers.splice(currentUserPosition, 1);
     fs.writeFile(
       'storage.json',
-      JSON.stringify({ users }),
+      JSON.stringify({ users: localUsers }),
       err => {
         throw new Error(err);
       }
     );
-    res.json(users);
+    res.json(localUsers);
     res.end();
   }
 });
