4028321de2e66c2c10abd138fcb8e7eca730c08a
ra44o <ra44elo@gmail.com>
2019-11-13 14:23:02 +0300

    fix: fix router's paths

--- express-server-with-architecture/index.js
+++ express-server-with-architecture/index.js
@@ -9,9 +9,6 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/users/get-users', router);
-app.use('/api/users/create-user', router);
-app.use('/api/users/update-user', router);
-app.use('/api/users/delete-user', router);
+app.use('/', router);
 
 app.listen(PORT, () => console.log(`Server on port ${PORT}`));
--- express-server-with-architecture/routers/myRouter.js
+++ express-server-with-architecture/routers/myRouter.js
@@ -3,9 +3,9 @@ const router = new express.Router();
 const MyController = require('../controllers/myController');
 const controller = new MyController();
 
-router.get('/', controller.getUsers);
-router.post('/', controller.createUser);
-router.put('/', controller.updateUser);
-router.delete('/', controller.deleteUser);
+router.get('/api/users', controller.getUsers);
+router.post('/api/users', controller.createUser);
+router.put('/api/users', controller.updateUser);
+router.delete('/api/users/:id', controller.deleteUser);
 
 module.exports = router;
