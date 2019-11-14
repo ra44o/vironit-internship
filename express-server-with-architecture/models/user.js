7757cb0e713bebc0e3e3e4f5586c8fa37936149d
ra44o <ra44elo@gmail.com>
2019-11-14 17:50:09 +0300

    fix: delete version key in scheme

--- express-server-with-architecture/models/user.js
+++ express-server-with-architecture/models/user.js
@@ -1,23 +1,27 @@
 const { Schema, model } = require('mongoose');
 
-const schema = new Schema({
-  id: {
-    type: Number,
-    required: true
+const schema = new Schema(
+  {
+    id: {
+      type: Number,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    surname: {
+      type: String,
+      required: true
+    },
+    isActive: {
+      type: Boolean,
+      default: false,
+      required: true
+    }
   },
-  name: {
-    type: String,
-    required: true
-  },
-  surname: {
-    type: String,
-    required: true
-  },
-  isActive: {
-    type: Boolean,
-    default: false,
-    required: true
-  }
-});
+  {
+    versionKey: false
+  });
 
 module.exports = model('User', schema);
