9b7025536d43032f2fb90dfb6a89cfd751cb42c8
ra44o <ra44elo@gmail.com>
2019-11-14 15:07:50 +0300

    feat: connect mongoDB

--- express-server-with-architecture/index.js
+++ express-server-with-architecture/index.js
@@ -2,6 +2,7 @@ const router = require('./routers/myRouter');
 const helmet = require('helmet');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,4 +12,27 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/', router);
 
-app.listen(PORT, () => console.log(`Server on port ${PORT}`));
+const start = async () => {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://ra44o:rak1997@vironit-intern-ayhnq.mongodb.net/users',
+      {
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      },
+      err => {
+        if (err) {
+          throw new Error(err);
+        }
+        console.log('Database is connected');
+      }
+    );
+    app.listen(PORT, () => console.log(`Server on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
