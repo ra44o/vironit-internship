3d4cd0c2c957362a5bad0f267e16418d53104514
ra44o <ra44elo@gmail.com>
2019-11-18 14:50:00 +0300

    fix: remove localUsers

--- express-server-with-architecture/services/myService.js
+++ express-server-with-architecture/services/myService.js
@@ -1,105 +1,50 @@
-const fs = require('fs');
-const path = require('path');
-
 const User = require('../models/user');
 
-let localUsers;
-
-const initializeDB = async () => {
-  localUsers = await User.find(
+const get = async () => {
+  return await User.find(
     { isActive: true },
     err => {
       if (err) {
         throw new Error('Can not get users');
       }
-    });
-}
-
-initializeDB();
-
-const get = async () => {
-  return localUsers;
+    }
+  );
 }
 
 const create = async requestBody => {
-  // we must pass the whole object to write it
-  if (!requestBody.id || !requestBody.name || !requestBody.surname || !requestBody.isActive) {
+  if (!requestBody.name || !requestBody.surname) {
     throw new Error("Bad request");
   } else {
-    if (localUsers.some(user => user.id === requestBody.id)) {
-      throw new Error(`User with id ${requestBody.id} exists`);
-    } else {
-      const newUser = new User({
-        id: requestBody.id,
-        name: requestBody.name,
-        surname: requestBody.surname,
-        isActive: requestBody.isActive
-      });
-      localUsers.push(newUser);
-      await newUser.save();
+    const newUser = new User({
+      name: requestBody.name,
+      surname: requestBody.surname
+    });
+    await newUser.save();
 
-      return 'User created';
-    }
+    return 'User created';
   }
 }
 
-const update = async requestBody => {
-  // here we must pass the id and can pass name and surname or both of them
-  if (!localUsers.some(user => user.id === requestBody.id)) {
-    throw new Error(`User with id ${requestBody.id} does not exist`);
-  } else {
-    const currentUser = localUsers.filter(user => user.id === requestBody.id)[0];
-
-    if (requestBody.name && requestBody.surname) {
-
-      currentUser.name = requestBody.name;
-      currentUser.surname = requestBody.surname;
-      await User.updateOne(
-        { id: currentUser.id },
-        {
-          $set: { "name": requestBody.name, "surname": requestBody.surname }
-        }
-      );
+const update = async (requestId, requestBody) => {
+  await User.updateOne(
+    { _id: requestId },
+    {
+      $set: { ...requestBody }
     }
-    else if (requestBody.name) {
-      currentUser.name = requestBody.name;
-      await User.updateOne(
-        { id: currentUser.id },
-        {
-          $set: { "name": requestBody.name }
-        }
-      );
-    } else if (requestBody.surname) {
-      currentUser.surname = requestBody.surname;
-      await User.updateOne(
-        { id: currentUser.id },
-        {
-          $set: { "surname": requestBody.surname }
-        }
-      );
-    }
+  );
 
-    return 'User updated';
-  }
+  return 'User updated';
 }
 
 const del = async requestId => {
-  // here we must pass the id of the user to delete it
-  const passedId = parseInt(requestId);
-  if (!localUsers.some(user => user.id === passedId)) {
-    throw new Error(`User with id ${passedId} does not exist`);
-  } else {
-    const currentUserPosition = localUsers.findIndex(user => user.id === passedId);
-    localUsers.splice(currentUserPosition, 1);
-    await User.updateOne(
-      { id: passedId },
-      {
-        $set: { "isActive": false }
-      }
-    );
+  await User.updateOne(
+    { _id: requestId },
+    {
+      $set: { "isActive": false }
+    }
+  );
 
-    return 'User deleted';
-  }
+  return 'User deleted';
 }
 
 module.exports = {
