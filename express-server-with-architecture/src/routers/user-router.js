8ea099857fa99fa2c0863a3cf4f712980308670c
ra44o <ra44elo@gmail.com>
2019-11-24 14:32:37 +0300

    feat: add login route

--- express-server-with-architecture/src/routers/user-router.js
+++ express-server-with-architecture/src/routers/user-router.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = new express.Router();
 const UserController = require('../controllers/user-controller');
 const controller = new UserController();
-const { validateUserCreate, validateUserUpdate } = require('../middlewares/validation/user-validation');
+const { validateUserCreate, validateUserUpdate, validateLogin } = require('../middlewares/validation/user-validation');
+const auth = require('../middlewares/authentication/auth');
 
 router.get('/', controller.getAllUsers);
 router.get('/:id', controller.getCertainUser);
 router.post('/', validateUserCreate, controller.createUser);
-router.put('/:id', validateUserUpdate, controller.updateUser);
-router.delete('/:id', controller.deleteUser);
+router.post('/login', validateLogin, controller.login);
+router.put('/:id', auth, validateUserUpdate, controller.updateUser);
+router.delete('/:id', auth, controller.deleteUser);
 
 module.exports = router;
