b96473c1019b617e7d079685fcc54e61fafbd826
ra44o <ra44elo@gmail.com>
2019-11-24 21:06:22 +0300

    feat: add ecnryption middleware

--- express-server-with-architecture/src/routers/user-router.js
+++ express-server-with-architecture/src/routers/user-router.js
@@ -3,13 +3,14 @@ const router = new express.Router();
 const UserController = require('../controllers/user-controller');
 const controller = new UserController();
 const { validateUserCreate, validateUserUpdate, validateLogin } = require('../middlewares/validation/user-validation');
-const auth = require('../middlewares/authentication/auth');
+const { authorize } = require('../middlewares/authentication/auth');
+const { encryptPass } = require('../middlewares/encryption/hash')
 
 router.get('/', controller.getAllUsers);
 router.get('/:id', controller.getCertainUser);
-router.post('/', validateUserCreate, controller.createUser);
+router.post('/', validateUserCreate, encryptPass, controller.createUser);
 router.post('/login', validateLogin, controller.login);
-router.put('/:id', auth, validateUserUpdate, controller.updateUser);
-router.delete('/:id', auth, controller.deleteUser);
+router.put('/:id', authorize, validateUserUpdate, encryptPass, controller.updateUser);
+router.delete('/:id', authorize, controller.deleteUser);
 
 module.exports = router;
