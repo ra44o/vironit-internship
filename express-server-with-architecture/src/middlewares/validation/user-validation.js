eb6b5c5750d944f36aba97883359b1b491fcd87d
ra44o <ra44elo@gmail.com>
2019-11-24 14:32:11 +0300

    fix-feat: fix create and update, add login method

--- express-server-with-architecture/src/middlewares/validation/user-validation.js
+++ express-server-with-architecture/src/middlewares/validation/user-validation.js
@@ -4,35 +4,54 @@ const createSchema = Joi.object().keys({
   name: Joi.string().trim().required(),
   surname: Joi.string().alphanum().trim().required(),
   isActive: Joi.boolean().falsy(''),
-  cityID: Joi.string().trim()
+  cityID: Joi.string().trim(),
+  login: Joi.string().min(3),
+  password: Joi.string().min(3)
 });
 
 const updateSchema = Joi.object().keys({
   name: Joi.string().trim(),
   surname: Joi.string().alphanum().trim(),
   isActive: Joi.boolean().falsy(''),
-  cityID: Joi.string().trim()
+  cityID: Joi.string().trim(),
+  login: Joi.string().min(3),
+  password: Joi.string().min(3)
+});
+
+const loginSchema = Joi.object().keys({
+  login: Joi.string().min(3).required(),
+  password: Joi.string().min(3).required()
 });
 
 const validateUserCreate = async (req, res, next) => {
   try {
-    await createSchema.validateAsync(req.body);
+    await createSchema.validate(req.body);
     next();
   } catch (err) {
-    res.status(400).send('Bad request');
+    res.status(400).send({ msg: err.message });
   }
 };
 
 const validateUserUpdate = async (req, res, next) => {
   try {
-    await updateSchema.validateAsync(req.body);
+    await updateSchema.validate(req.body);
     next();
   } catch (err) {
-    res.status(400).send('Bad request');
+    res.status(400).send({ msg: err.message });
+  }
+};
+
+const validateLogin = async (req, res, next) => {
+  try {
+    await loginSchema.validate(req.body);
+    next();
+  } catch (err) {
+    res.status(403).send({ msg: err.message });
   }
 };
 
 module.exports = {
   validateUserCreate,
-  validateUserUpdate
+  validateUserUpdate,
+  validateLogin
 };
